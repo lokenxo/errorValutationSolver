78d0634381fb7b14795dea51b9be7954bac86271
Lorenzo <thisloke@gmail.com>
2016-03-11 10:45:16 +0100

    mi sto divertendo proprio male! La grafica dei nodi dell'albero è stata associata a cerchi. Ho aggiunto le parentesi alle label, così si capisce meglio la precedenza

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,10 +15,47 @@ angular.module('errorValutationSolverApp').controller('MainCtrl', function ($sco
    $scope.valuta = function()
    {
    		$scope.eval= jsep($scope.inputbox.value);
-   		treeGenerator.createTree($scope.eval, function()
-		{
-			console.log('albero creato');	
-		});
+   	  treeGenerator.createTree($scope.eval, $scope.inputbox.value,function(lista)
+  		{
+
+      	console.log('albero creato');	
+        var render = function(r, n) 
+        {
+          var label = r.text(0, 0, n.label).attr({opacity:1});
+          label.attr({'font-size':14});
+          //the Raphael set is obligatory, containing all you want to display 
+          var set = r.set()
+            .push(r.circle(0, 0, 0)
+              .attr({ fill: '#fafafa', 'stroke-width': 1, r: 50 })
+            )
+            .push(label);
+
+          return set;
+        };
+
+        var g = new Graph();
+
+        //Aggiungo l'elento grafico nodo
+        for(var i =0; i < lista.length; i++)
+        {
+          g.addNode(lista[i].id, {label: lista[i].label, render: render});
+        }
+
+        //Aggiungo l'elemento grafico arco
+        for(var i =0; i < lista.length; i++)
+        {
+          if(lista[i].parent!=null)
+          {
+            g.addEdge(lista[i].id, lista[i].parent);
+          }
+        }
+        
+        var layouter = new Graph.Layout.Spring(g);
+        layouter.layout();
+         
+        var renderer = new Graph.Renderer.Raphael('canvas', g, 600, 600);
+        renderer.draw();
+      });
    }
 
   });
--- app/scripts/services/treegenerator.js
+++ app/scripts/services/treegenerator.js
@@ -11,17 +11,137 @@ angular.module('errorValutationSolverApp').service('treeGenerator', function ()
 {
 	var service = {};
 
-	service.createTree = function(treeFile, callback)
+
+  	//visita a partire dalle foglie a ritroso fino alla radice, così propaga all'indietro i valori valutati
+  	function setValueOfLeaves(lista)
+  	{
+  		var coda = [];
+  		var list = [];
+
+  		//Setto le label a tutte le foglie e le inserisco in coda
+  		for(var i =0; i< lista.length; i++)
+  		{
+  			var elem = lista[i];
+  			if(elem.left==null && elem.right==null)
+  			{
+  				if(elem.type=="Identifier")
+		 		{
+					elem.label=elem.name;
+				}
+		 		if(elem.type=="Literal")
+		 		{
+	  				elem.label=elem.raw;
+				}
+				coda.push(elem);
+				list.push(elem);
+  			}
+  		}
+  		console.log(coda);
+  		while(coda.length!=0)
+  		{
+  			//Per ogni valore della coda, visito il parent e lo inserisco in coda, se non è stato già visitato, cioè è in list
+  			var c = coda[0];
+  			//console.log(c);
+  			for(var j=0; j<lista.length; j++)
+  			{
+  				//console.log(lista[j]);
+  				if(c.parent == lista[j].id)
+  				{
+  					console.log('trovato parent');
+  					//Ecco il parent, calcolo il valore se non è già in lista
+  					coda.push(lista[j]);
+  					if(c.type=="BinaryExpression")
+			 		{
+			 			console.log('binart');
+			 			if(c.left.type=='BinaryExpression' && c.right.type=='BinaryExpression')
+			 			{
+			 				c.label='('+c.left.label+')'+c.operator+'('+c.right.label+')';
+			 			}
+			 			if(c.left.type=='BinaryExpression' && c.right.type!='BinaryExpression')
+			 			{
+			 				c.label='('+c.left.label+')'+c.operator+c.right.label;
+			 			}
+			 			if(c.left.type!='BinaryExpression' && c.right.type=='BinaryExpression')
+			 			{
+			 				c.label=c.left.label+c.operator+'('+c.right.label+')';
+
+			 			}
+			 			if(c.left.type!='BinaryExpression' && c.right.type!='BinaryExpression')
+			 			{
+			 				c.label=c.left.label+c.operator+c.right.label;
+			 			}
+		  				
+		  				list.push(c);
+		  				break;
+					}
+  				}
+  				if(c.parent==null)
+  				{
+  					//l'elemento radice
+  					if(c.type=="BinaryExpression")
+			 		{
+			 			console.log('binartRadice');
+			 			if(c.left.type=='BinaryExpression' && c.right.type=='BinaryExpression')
+			 			{
+			 				c.label='('+c.left.label+')'+c.operator+'('+c.right.label+')';
+			 			}
+			 			if(c.left.type=='BinaryExpression' && c.right.type!='BinaryExpression')
+			 			{
+			 				c.label='('+c.left.label+')'+c.operator+c.right.label;
+			 			}
+			 			if(c.left.type!='BinaryExpression' && c.right.type=='BinaryExpression')
+			 			{
+			 				c.label=c.left.label+c.operator+'('+c.right.label+')';
+
+			 			}
+			 			if(c.left.type!='BinaryExpression' && c.right.type!='BinaryExpression')
+			 			{
+			 				c.label=c.left.label+c.operator+c.right.label;
+
+			 			}
+			 		}
+  				}
+  			}
+  			//coda.push(c);
+  			coda=coda.slice(1,coda.length);
+  		}
+
+  		//La lista va bene, devo eliminare i duplicati
+  		var listNoDuplicates = [];
+  		for(var i=0; i< list.length;i++)
+  		{
+  			var trovato = false;
+  			for(var j=0; j< listNoDuplicates.length; j++)
+  			{
+  				if(list[i].id==listNoDuplicates[j].id)
+  				{
+  					trovato=true;
+  				}
+  			}
+  			if(!trovato)
+  			{
+  				//Aggiungo
+  				listNoDuplicates.push(list[i]);
+  			}
+  		}
+  		
+  		return listNoDuplicates;
+  	}
+
+
+	service.createTree = function(treeFile, func, callback)
 	{
 
 		//Aggiungi i figli alla pila e dagli un id univoco
 		treeFile.id=0;
-
+		treeFile.label=func;
+		var x = treeFile;
 		var coda = [];
 		var lista = [];
 
 		coda.push(treeFile);
 		var identifier=0;
+
 	 	while(coda.length!=0)
 	 	{
 
@@ -46,41 +166,10 @@ angular.module('errorValutationSolverApp').service('treeGenerator', function ()
 	 		lista.push(elem);
 		}
 
-		var g = new Graph();
-
-		for(var i =0; i < lista.length; i++)
-		{
-
-			if(lista[i].type=="BinaryExpression")
-	 		{
-	 			g.addNode(lista[i].id, {label: lista[i].operator});
-	 		}
-	 		else
-	 		{
-	 			if(lista[i].type=="Identifier")
-		 		{
-					g.addNode(lista[i].id, {label: lista[i].name});		 		
-				}
-		 		else
-		 		{
-					g.addNode(lista[i].id, {label: lista[i].raw});		 		
-				}
-	 		}
-	 		if(lista[i].parent!=null)
-	 		{
-	 			g.addEdge(lista[i].id, lista[i].parent);	
-	 		}
-	 		
-
-	 	}
+		lista = setValueOfLeaves(lista);
+		lista.push(x);
 		
-		var layouter = new Graph.Layout.Spring(g);
-		layouter.layout();
-		 
-		var renderer = new Graph.Renderer.Raphael('canvas', g, 600, 700);
-		renderer.draw();
-		
-		return callback();
+		return callback(lista);
 	}
 
 	return service;
